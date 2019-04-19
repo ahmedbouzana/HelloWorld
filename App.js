ca1e92e410e232421f1ddd04036d0c37b9845761
ahmedbouzana <ah.bouzana@gmail.com>
2019-04-19 23:49:32 +0200

    Work with Stylesheets

--- App.js
+++ App.js
@@ -8,49 +8,25 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-import { throwStatement } from "@babel/types";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {};
     this.state.customStyles = {
       color: "red"
     };
-
-    setInterval(() => {
-      if (this.state.customStyles.color == "red") {
-        this.setState({
-          customStyles: {
-            color: "green"
-          }
-        });
-      } else {
-        this.setState({
-          customStyles: {
-            color: "red"
-          }
-        });
-      }
-    }, 1000);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={[styles.welcome, this.state.customStyles]}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>To get started, edit App.js.</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.half1}>
+          <Text style={styles.text}>This is style half1</Text>
+        </View>
+        <View style={styles.half2}>
+          <Text>This is style half2</Text>
+        </View>
       </View>
     );
   }
@@ -59,19 +35,20 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row"
+  },
+  half1: {
+    flex: 1,
+    backgroundColor: "green",
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    alignItems: "center"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-    color: "blue"
+  half2: {
+    flex: 1,
+    backgroundColor: "yellow"
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  text: {
+    color: "white",
+    fontSize: 20
   }
 });
